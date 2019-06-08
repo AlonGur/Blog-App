c7031d2ccf12560452de359f90cdfd75bf2f6638
alongur <alongur@github.com>
2019-06-08 14:37:14 +0300

    styling updated

--- src/app/button.directive.ts
+++ src/app/button.directive.ts
@@ -14,7 +14,7 @@ export class ButtonDirective implements OnInit {
   }
 
   ngOnInit(){
-    this.elementRef.nativeElement.style.backgroundColor='yellow';
+    //this.elementRef.nativeElement.style.backgroundColor='yellow';
     this.elementRef.nativeElement.style.color= 'black';
 
     this.elementRef.nativeElement.style.borderRadius='10px';
